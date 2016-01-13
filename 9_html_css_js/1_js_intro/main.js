be4ce40ccf47c4dc5ec043c9f567a02938523cdc
Jason <Jcory.ng@gmail.com>
2016-01-13 13:22:07 -0500

    js intro done

---
+++ 9_html_css_js/1_js_intro/main.js
@@ -0,0 +1,15 @@
+// var name = process.argv[2]
+
+// console.log(name)
+
+var a = "Hello";
+var b = "World";
+
+console.log(a + " " + b)
+
+console.log(a,b)
+
+var x = "Beautiful "
+var y = "People"
+
+console.log(x.concat(y))
