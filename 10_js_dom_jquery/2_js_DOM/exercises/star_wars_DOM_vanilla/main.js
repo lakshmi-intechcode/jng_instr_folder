0d4ade4d1ae18a460413302c31fc94747a3abdac
Jason <Jcory.ng@gmail.com>
2016-01-13 17:28:39 -0500

    making some more front end hw

---
+++ 10_js_dom_jquery/2_js_DOM/exercises/star_wars_DOM_vanilla/main.js
@@ -0,0 +1,45 @@
+// Obi Wan finds Luke Skywalker on Tatooine
+// Obi Wan and Luke go on the Millenium Falcon with Han Solo and Chewbacca
+// Obi Wan, Luke, Han, Chewy go to the death star
+// They find Leia on the Death Star
+// Obi Wan gets struck down by Darth Vader
+// Luke, Han, Chewy, and Leia go to the Millenium Falcon
+// Luke Skywalker gets on an X-wing
+// Luke goes to the Death Star
+// Darth Vader gets in a Tie Fighter
+// Han and Chewy go to the Death Star
+// Darth Vader gets shot down by Han and Chewy
+// Luke blows up the Death Star
+
+var galaxy_places = [
+	"Tatooine",
+	"Millenium Falcon",
+	"Death Star",
+	"X-wing",
+	"Tie Fighter"
+]
+
+var jedi = [
+	"Luke Skywalker",
+	"Obi Wan Kenobi"
+];
+
+var heroes = [
+	"Han Solo",
+	"Chewbacca",
+	"Leia Organa"
+];
+
+var darkside = [
+	"Darth Vader"
+];
+
+
+
+
+
+
+
+
+
+
