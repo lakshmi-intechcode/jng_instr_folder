3c184b6892ed60649767f26e4bf58817304d4f8b
Jason <Jcory.ng@gmail.com>
2016-02-11 16:21:47 -0500

    added thursday prezzy folder

---
+++ Thur_Prezzies/whiskey_modal/main.js
@@ -0,0 +1,21 @@
+$(".open-modal").on("click", function() {
+  console.log(this.id)
+  // blantons()
+  modal_func(this.id)
+});
+
+$("#close").on("click", function() {
+  $(".modal").toggle();
+});
+
+var modal_func = function(w){
+	console.log(whiskeys[w])
+	var heading = whiskeys[w].heading;
+	var describe = whiskeys[w].describe;
+	var image = whiskeys[w].image;
+
+	$("h3").html(heading);
+  $(".describe").html(describe)
+  $("img").attr("src", image)
+  $(".modal").toggle();
+}
