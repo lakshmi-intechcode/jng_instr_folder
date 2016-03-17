374b2614295a0af66ef9f9ddc3c0adf02fb325ec
Jason <Jcory.ng@gmail.com>
2016-03-17 10:27:51 -0400

    pushing solutions to brain teasers

---
+++ 1_python_fundamentals/3_some_solutions/currency/currency_solution.py
@@ -0,0 +1,17 @@
+import math
+
+def currency_converter(amount):
+	denominations = [.01, .05, .1, .25, 1, 5, 10, 20, 50, 100]
+	current_bill = 0
+	bill_amount = 0
+	wallet = dict()
+
+	while len(denominations) > 0:
+		current_bill = denominations.pop()
+		bill_amount = math.floor(amount/current_bill)
+		wallet[current_bill] = bill_amount
+		amount = amount - (current_bill * bill_amount)
+
+	return wallet
+
+print(my_money(23.45))
