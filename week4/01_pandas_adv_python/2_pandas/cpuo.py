a3d79c5b7a60e05a33dbb061a591480afcfc938e
Jason <Jcory.ng@gmail.com>
2016-12-13 10:59:17 -0500

    edit pandas example

--- week4/01_pandas_adv_python/2_pandas/cpuo.py
+++ week4/01_pandas_adv_python/2_pandas/cpuo.py
@@ -3,9 +3,10 @@ import pandas as pd
 df = pd.read_csv('cpuo.csv')
 
 # print(df)
+# print(df.describe())
+# print(df.info())
 
 # print(df.head(n=2))
-
 # print(df.tail(n=2))
 
 # print(df['Ranking'])
