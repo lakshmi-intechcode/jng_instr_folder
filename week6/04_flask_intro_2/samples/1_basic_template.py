a93be6c2449311ee85e1b8019cb3d132a0fba003
Jason <Jcory.ng@gmail.com>
2017-01-12 11:10:25 -0500

    asodijf

--- week6/04_flask_intro_2/samples/1_basic_template.py
+++ week6/04_flask_intro_2/samples/1_basic_template.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def raw_template():
-    return render_template('1_basic_sample.html', name='samantha')
+    return render_template('1_basic_sample.html', name='Scarlett Johansson')
 
 if __name__ == "__main__":
     app.run(debug=True)
